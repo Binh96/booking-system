e38ca9bf0ee485b2798fbf063d54b566f53a396d
Binhpx <phanxuanbinh96@gmail.com>
2026-04-09 23:22:30 +0700

    [fix] change file

--- src/models/booking/domain/repositories/booking-repository.ts
+++
@@ -1,4 +0,0 @@
-export abstract class BookingRepository {
-  abstract saveBooking(booking: any): Promise<void>;
-  abstract findOverLappingBookings(startDate: Date, endDate: Date): Promise<any[]>;
-}
